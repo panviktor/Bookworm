75a354b1048f9ab0541ccf8456f6ec597bd400cd
Panviktor <panafviktor@gmail.com>
2020-06-19 14:37:11 +0300

    Initial Commit

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Виктор on 19.06.2020.
+//  Copyright © 2020 SwiftViktor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
